6f67d28d77044438022220ee4f4305b927c3ab5c
Moustikos <remi.pretre@gmail.com>
2020-04-23 12:30:17 +0000

    Update project information

--- webapp/util/Firebase.js
+++ webapp/util/Firebase.js
@@ -9,13 +9,13 @@ sap.ui.define(["sap/ui/model/json/JSONModel"], function (JSONModel) {
 		initializeFirebase: function () {
 			// Initialize Firebase with the Firebase-config
 			firebase.initializeApp({
-				apiKey: "AIzaSyAbc5mV1vOPv4MlHq9lcWb262dv17HDATU",
-				authDomain: "blindtest-7e6df.firebaseapp.com",
-				databaseURL: "https://blindtest-7e6df.firebaseio.com",
-				projectId: "blindtest-7e6df",
-				storageBucket: "blindtest-7e6df.appspot.com",
-				messagingSenderId: "117145039765",
-				appId: "1:117145039765:web:87b1832551c0c55812bc9c"
+				apiKey: "AIzaSyDveJ9QTgFoW0KiKIdMcaff6m6LewjmoYo",
+			    authDomain: "belote-a1e70.firebaseapp.com",
+			    databaseURL: "https://belote-a1e70.firebaseio.com",
+			    projectId: "belote-a1e70",
+			    storageBucket: "belote-a1e70.appspot.com",
+			    messagingSenderId: "451575044085",
+			    appId: "1:451575044085:web:ac0ba8d8b5bcdf5ac172bc"
 			});
 		}
 	};

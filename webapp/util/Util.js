eb1bfcabd5ebf90874c1aefeb1cd363e36bb63dd
Moustikos <remi.pretre@gmail.com>
2020-05-02 13:01:58 +0000

    Correction

--- webapp/util/Util.js
+++ webapp/util/Util.js
@@ -103,7 +103,7 @@ sap.ui.define([], function () {
 			if (oEntry.NTeams[1].NFolds) {
 				var aKeysNFolds2 = Object.keys(oEntry.NTeams[1].NFolds);
 				for (var f2 = 0; f2 < aKeysNFolds2.length; f2++) {
-					aFoldTeam1.push({
+					aFoldTeam2.push({
 						"Name": oEntry.NTeams[0].NFolds[aKeysNFolds2[f2]].cardName
 					});
 				}
